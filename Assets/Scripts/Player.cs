c1a878de2cb5d30a904fd1fdec4b9d43ad220f75
Saeed Gatson <saeedgatson@gmail.com>
2015-07-05 11:54:48 -0700

    Support Keyboard And Mobile Input Together When In Editor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -39,7 +39,33 @@ public class Player : MovingObject {
 		int horizontal = 0;
 		int vertical = 0;
 
- 	#if UNITY_STANDALONE || UNITY_WEBPLAYER
+	#if UNITY_EDITOR
+		if(Input.touchCount > 0) {
+			Touch myTouch = Input.touches[0];
+			
+			if(myTouch.phase == TouchPhase.Began) {
+				touchOrigin = myTouch.position;
+			}
+			else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
+				Vector2 touchEnd = myTouch.position;
+				float x = touchEnd.x - touchOrigin.x;
+				float y = touchEnd.y - touchOrigin.y;
+				touchOrigin.x = -1;
+				if(Mathf.Abs(x) > Mathf.Abs(y))
+					horizontal = x > 0 ? 1 : -1;
+				else
+					vertical = y > 0 ? 1: -1;
+			}
+		} else {
+			horizontal = (int)Input.GetAxisRaw ("Horizontal");
+			vertical = (int)Input.GetAxisRaw ("Vertical");
+			
+			if (horizontal != 0) {
+				vertical = 0;
+			}
+		}
+
+	#elif  UNITY_STANDALONE || UNITY_WEBPLAYER
 
 		horizontal = (int)Input.GetAxisRaw ("Horizontal");
 		vertical = (int)Input.GetAxisRaw ("Vertical");
@@ -48,7 +74,7 @@ public class Player : MovingObject {
 			vertical = 0;
 		}
 
-	#else
+	#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 
 		if(Input.touchCount > 0) {
 			Touch myTouch = Input.touches[0];
@@ -68,7 +94,7 @@ public class Player : MovingObject {
 			}
 		}
 
-		#endif
+	#endif
 
 		if (horizontal != 0 || vertical != 0) {
 			AttemptMove<Wall> (horizontal, vertical);
